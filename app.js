5dd669343bf548e7d36b6ea6d96d7220531776ff
Gamid <gamidi@yandex.ru>
2022-11-30 15:27:20 +0300

    777

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require('express'); 
-const app = express(); 
-require('@babel/register');
-const morgan = require('morgan'); 
-const path = require('path');
-require('dotenv').config(); 
+const express = require('express');
 
-//импорт вспомогательных ф-й
+const app = express();
+require('@babel/register');
+const morgan = require('morgan');
+const path = require('path');
+require('dotenv').config();
+
+// импорт вспомогательных ф-й
 const dbCheck = require('./db/dbCheck');
 
 // импорт роутов
 const indexRoutes = require('./routes/indexRoutes');
 
- // вызов функции проверки соединения с базоый данных
+// вызов функции проверки соединения с базоый данных
 dbCheck();
 
 app.use(express.static(path.resolve('public')));
@@ -19,11 +20,11 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//роутеры
+// роутеры
 app.use('/', indexRoutes);
 
 const PORT = process.env.PORT || 3100;
 app.listen(PORT, (err) => {
-  if (err) return console.log('Ошибка запуска сервера.', err.message)
+  if (err) return console.log('Ошибка запуска сервера.', err.message);
   console.log(`Сервер запущен на http://localhost:${PORT} `);
 });
